ecf852ffa18cd6966f3287ced71e83dd7cb94584
Akash Zalke <akash.v.zalke@kipi.bi>
2024-05-15 11:01:02 +0000

    Commit

--- models/consumption_layer/ALL_SOURCE_EVENTS.sql
+++ models/consumption_layer/ALL_SOURCE_EVENTS.sql
@@ -8,9 +8,27 @@
 
 
 with cte as(
-select * 
+select "requestID" as REQUESTID ,
+        event_date as EVENT_DATE,
+        "source_name" as SOURCE_NAME,
+        "sourceIPAddress" as SOURCEIPADDRESS,
+        result as RESULT,
+        userid as USERID,
+        name as NAME,
+       case 
+       when RESULT = 'FAILURE' then ABS(random())%5 + 1 
+       else 0
+       END AS SEVERITY
+       from {{ref('transformed')}}
 )
-select * 
+select * , 
+    case 
+    when ABS(random())%5 + 1  = 1 then 'MALWARE' 
+    when ABS(random())%5 + 1  = 2 then 'FAILED LOGIN'
+    when ABS(random())%5 + 1  = 3 then 'SUSPICIOUS ACTIVITY'
+    when ABS(random())%5 + 1  = 4 then 'PHISING'
+    else 'CLOUDSECURITY ALERTS'
+    end as CATEGORY
     from cte
 
        
